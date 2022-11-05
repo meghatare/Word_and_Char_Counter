ab6d47aa0f06ea41db27dbae92d67094b5907eea
Megha Tare <meghatare123@gmail.com>
2022-11-05 20:27:51 +0530

    add a trim function to word & char

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ let textbox = document.getElementById("textbox");
             let text = this.value;
             let char = text.length;
             document.getElementById("char").innerHTML = char;
+
+            text = text.trim();
+            let word = text.split(" ");
+            let cleanList = word.filter(function(elm){
+                return elm != "";
+            });
+            console.log(cleanList);
+            document.getElementById("word").innerHTML = cleanList.length;
         })
